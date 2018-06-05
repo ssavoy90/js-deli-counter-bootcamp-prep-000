fc2765cd2309ef02abfc3b09fa4e873a4e6f664c
Shanelle Savoy <mznellienellz@gmail.com>
2018-06-05 13:12:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(katzDeliLine, newPerson) {
   var katzDeli = [];
   var count = 0
   while(count < katzDeliLine.length) {
-    katzDeli.push(katzDeliLine[count]);
+    katzDeli.push("Welcome" + " " + katzDeliLine[count]);
     count++;
   }
   return katzDeli
