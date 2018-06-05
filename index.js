e9a830570c62701ae1b3d2f5b58df71ce0f0417b
Shanelle Savoy <mznellienellz@gmail.com>
2018-06-05 14:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,3 @@ function takeANumber(katzDeliLine, newPerson) {
  return(`Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`);
 }
 
- function nowServing(newPerson) {
-   return newPerson
-   newPerson.pop(newPerson)
-   return(`There is nobody waiting to be served!`)
- }
